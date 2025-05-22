325472eea59667064252cd0aa6d26135b308a7cb
Petro <petrotymchuck@gmail.com>
2025-05-22 19:59:50 +0300

    new validation option

--- RestaurantWebAPI/Validators/Category/CategoryCreateValidator.cs
+++ RestaurantWebAPI/Validators/Category/CategoryCreateValidator.cs
@@ -1,17 +1,22 @@
 ﻿using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+using RestaurantWebAPI.Data;
 using RestaurantWebAPI.Models.Category;
 
 namespace RestaurantWebAPI.Validators.Category;
 
 public class CategoryCreateValidator : AbstractValidator<CategoryCreateModel>
 {
-    public CategoryCreateValidator()
+    public CategoryCreateValidator(AppDbRestaurantContext db)
     {
         RuleFor(x => x.Name)
             .NotEmpty()
             .WithMessage("Ім'я є обов'язковим.")
             .Length(1, 250)
             .WithMessage("Ім'я не може бути довшим за 250 символів.");
+            //.MustAsync(async (name, cancellation) =>
+            //!await db.Categories.AnyAsync(c => c.Name == name, cancellation))
+            //.WithMessage("Категорія з такою назвою вже існує");
         RuleFor(x => x.Slug)
             .NotEmpty()
             .WithMessage("Слаг є обов'язковим.")
